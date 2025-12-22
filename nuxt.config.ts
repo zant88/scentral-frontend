ae25f64af6f5da69b34ace377e08bc0b9beb2f78
Ehsan Basri <ehsan.basri88@gmail.com>
2025-12-22 15:29:30 +0700

    change endoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,8 +46,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      // apiBase: 'https://office.scentralhub.com',
-      apiBase: 'http://192.168.18.137:3030',
+      apiBase: 'https://office.scentralhub.com',
+      // apiBase: 'http://192.168.18.137:3030',
       mqttBase: 'wss://mqtt.kodemake.com',
       mqttUsername: 'scentral',
       mqttPassword: 'ar0m4db'
